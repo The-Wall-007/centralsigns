1e0b6653f8441a407ae40cff70ccf72ad68f0865
The-Wall-007 <dvlpatel2410@gmail.com>
2023-06-14 16:18:24 +0530

    done header section having poster video logo and contents

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Header = (props) => {
+  const { props: propsValues, children } = props;
+
+  const [state] = useState({
+    video: "assets/videos/header.mp4",
+    poster: "assets/images/poster.png",
+    logo: "assets/images/logo.png",
+  });
+
+  return (
+    <div className="header">
+      <div className="container pr">
+        <div className="header__logo">
+          <img src={state.logo} alt="Logo" />
+        </div>
+      </div>
+      <div className="header__video">
+        <video
+          src={state.video}
+          autoPlay
+          loop
+          muted
+          poster={state.poster}
+        ></video>
+      </div>
+
+      <div className="header__contents">
+        <div className="container">
+          <div className="header__contents__text">
+            <div className="header__contents__text__child">
+              <h1 className="header__contents__text__child__h1">
+                {propsValues.heading}
+              </h1>
+
+              <p className="header__contents__text__child__p">
+                {propsValues.paragraph}
+              </p>
+
+              <div className="header__contents__text__child__link">
+                {children}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Header } from "../../components";
 
 const Home = (props) => {
-  return <div>This is Home page</div>;
+  const [state] = useState({
+    heading: ` WE CREATE COLORFUL`,
+    paragraph: ` PRINTING SOLUTIONS WITH UNBEATABLE QUALITY`,
+  });
+
+  return (
+    <div>
+      <Header props={state}>
+        <button className="btn-default"> Request a Quate</button>
+      </Header>
+    </div>
+  );
 };
 
 export default Home;
