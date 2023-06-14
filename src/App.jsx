375144d2f9bfaa1e222a09a2e3a230c0aa4ae3ed
The-Wall-007 <dvlpatel2410@gmail.com>
2023-06-14 12:55:41 +0530

    setup-routing with home-contact-about-notFound pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { About, Contact, Home, NotFound } from "./pages";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Project Settings</h1>
-      <p>font-size margin padding testing</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
